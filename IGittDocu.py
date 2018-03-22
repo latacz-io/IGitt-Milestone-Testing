5f936a32963fc0adf99568439db8b5deba26c7bf
Sebastian Latacz <sebastian@gitmate.io>
2018-03-22 15:11:34 +0100

    Zu implementierende Attribute durch das Interface auswählen

---
+++ IGittDocu.py
@@ -0,0 +1,8 @@
+IGitt.Utils.__init__.py CachedDataMixin #Collects all the data and writes it into self.data
+
+#Common Errors
+401: Unauthorized #Are you sure you are using the right token (Oauth/Private)
+
+#Interface
+Dictates the attributes being used in igitt
+--> When writing a new implementation, you need a getter and a setter for every attribute mentioned in the Interface
